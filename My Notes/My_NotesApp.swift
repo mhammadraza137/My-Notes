0ab452d216668e785e6283936be6829c67789c0f
mhammadraza137 <hammadrazamuhammad137@gmail.com>
2024-08-10 15:48:42 +0500

    Initial Commit

---
+++ My Notes/My_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_NotesApp.swift
+//  My Notes
+//
+//  Created by Septem Systems on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct My_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
